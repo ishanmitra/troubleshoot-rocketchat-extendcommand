cae59c99b780d31edf6bb0a9d76f4be974bd3a59
Ishan Mitra <iamishan1996@gmail.com>
2025-03-07 13:56:56 +0530

    First commit: uploading code followed from the developer documentation

---
+++ ExtendCommandApp.ts
@@ -0,0 +1,18 @@
+import {
+    IAppAccessors,
+    IConfigurationExtend,
+    ILogger,
+} from '@rocket.chat/apps-engine/definition/accessors';
+import { App } from '@rocket.chat/apps-engine/definition/App';
+import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { ExtendMessageCommand } from './commands/ExtendMessageCommand'; // [1]
+
+export class ExtendCommandApp extends App {
+    constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
+        super(info, logger, accessors);
+    }
+
+    public async extendConfiguration(configuration: IConfigurationExtend) {
+        configuration.slashCommands.provideSlashCommand(new ExtendMessageCommand()); // [2]
+    }
+}
